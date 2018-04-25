09e7c0e401a8100034cfed3c8490ec005615d919
Samuel Kaessner <sam.kaessner@me.com>
2018-04-25 14:02:33 -0600

    Finished code.

--- model/src/main/scala/ModelGenerator.scala
+++ model/src/main/scala/ModelGenerator.scala
@@ -5,7 +5,10 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer, VectorAssembler}
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer, VectorAssembler, StringIndexerModel}
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml._
 
 
 object ModelGenerator
@@ -15,132 +18,259 @@ object ModelGenerator
     {
         // Create new spark conf
         val sparkConf = new SparkConf()
-            .setMaster("spark://columbia:47401")
-            .setAppName("Model Generator")
-        
+          .setMaster("local")
+          //.setMaster("spark://columbia:47401")
+          .setAppName("Model Generator")
+
         // Create new spark session
         val sparkSession = SparkSession
-            .builder()
-            .appName("QualityMapper")
-            .config(sparkConf)
-            .getOrCreate()
-        
-        // Load in data set csv (argument 1) as a data frame
-        val rawDataFrame = sparkSession
-            .read
-            .format("csv")
-            .option("header", "true")
-            .load("hdfs://columbia:47481/data/THOR_Vietnam_Bombing_Operations.csv")
-        val validPredictors = rawDataFrame.na.drop()
+          .builder()
+          .appName("QualityMapper")
+          .config(sparkConf)
+          .getOrCreate()
 
-        
-        val labelIndexer = new StringIndexer()
-                        .setInputCol("VALID_AIRCRAFT_ROOT")
-                        .setOutputCol("labels")
-                        .fit(validPredictors)
-                        
-        val countryIndexed = new StringIndexer()
-                        .setInputCol("COUNTRYFLYINGMISSION")
-                        .setOutputCol("countryIndexed")
-                        //.setMaxCategories(4)
-                        
-        val assembler  = new VectorAssembler()
-                        .setInputCols(Array("countryIndexed"))
-                        .setOutputCol("features")
-                        
-        /*val targetIndexed = new VectorIndexer()
-                                .setInputCol("TGTTYPE")
-                                .setOutputCol("targetIndexed")*/
-        /*val weaponIndexed = new VectorIndexer()
-                                .setInputCol("WEAPONTYPE")
-                                .setOutputCol("weaponIndexed")
-        val countryIndexed = new VectorIndexer()
-                                .setInputCol("FLTHOURS")
-                                .setOutputCol("countryIndexed")
-        val countryIndexed = new VectorIndexer()
-                                .setInputCol("PERIODOFDAY")
-                                .setOutputCol("countryIndexed")
-        val countryIndexed = new VectorIndexer()
-                                .setInputCol("TGTWEATHER")
-                                .setOutputCol("countryIndexed")
-        val countryIndexed = new VectorIndexer()
-                                .setInputCol("TGTCLOUDCOVER")
-                                .setOutputCol("countryIndexed")
-        val countryIndexed = new VectorIndexer()
-                                .setInputCol("WEAPONSLOADEDWEIGHT")
-                                .setOutputCol("countryIndexed")*/
-                                
-                                
-                                
-        
-        
-        val Array(trainingData, testData) = validPredictors.randomSplit(Array(0.7, 0.3))
-        
-        // Train a RandomForest model.
-        val rf = new RandomForestClassifier()
-                        .setLabelCol("labels")
-                        .setFeaturesCol("features")
-                        .setNumTrees(10)
 
-        // Convert indexed labels back to original labels.
-        val labelConverter = new IndexToString()
-                        .setInputCol("prediction")
-                        .setOutputCol("PREDICTED_AIRCRAFT")
-                        .setLabels(labelIndexer.labels)
-                        
-        val pipeline = new Pipeline()
-                        .setStages(Array(labelIndexer, countryIndexed, assembler, rf, labelConverter))
-                        
-        val model = pipeline.fit(trainingData)
-        val predictions = model.transform(testData)
-        
-        predictions.select("PREDICTED_AIRCRAFT", "VALID_AIRCRAFT_ROOT", "features").show(5)
-        
-                        
-                        
-        // Select (prediction, true label) and compute test error
-        val evaluator = new MulticlassClassificationEvaluator()
-                        .setLabelCol("PREDICTED_AIRCRAFT")
-                        .setPredictionCol("VALID_AIRCRAFT_ROOT")
-                        .setMetricName("accuracy")
-                        
-        val accuracy = evaluator.evaluate(predictions)
-        println("Test set accuracy = " + accuracy)
-        
-        //val rmse = evaluator.evaluate(predictions)
-        //println("Root Mean Squared Error (RMSE) on test data = " + rmse)
-
-        //val treeModel = model.stages(1).asInstanceOf[DecisionTreeRegressionModel]
-        //println("Learned regression tree model:\n" + treeModel.toDebugString)
-        /*(
-        //First model
-        val aircraftTargets = rawDataFrame.select("VALID_AIRCRAFT_ROOT").show();
-        
-        val aircraftPredictors = rawDataFrame.select("COUNTRYFLYINGMISSION", 
-                                             "TGTTYPE", 
-                                             "WEAPONTYPE", 
-                                             "FLTHOURS",
-                                             "PERIODOFDAY", 
-                                             "TGTWEATHER",
-                                             "TGTCLOUDCOVER",
-                                             "WEAPONSLOADEDWEIGHT")
-                                             
-        validPredictors.show()
-        println(validPredictors.count())
-            */
-        
-        /*
-        //Second model
-        val weaponTarget = rawDataFrame.select("WEAPONTYPE").show();
-        
-        val weaponPredictors = rawDataFrame.select("COUNTRYFLYINGMISSION", 
-                                             "TGTTYPE", 
-                                             "VALID_AIRCRAFT_ROOT", 
-                                             "FLTHOURS",
-                                             "PERIODOFDAY", 
-                                             "TGTWEATHER",
-                                             "TGTCLOUDCOVER",
-                                             "WEAPONSLOADEDWEIGHT").show(); */
+      if (args(0) == "create") {
+        createAndSaveAircraftModel(sparkConf, sparkSession)
+      } else if (args(0) == "use") {
+        useAircraftModel(sparkSession)
+      }
     }
-    
+
+    def useAircraftModel(sparkSession: SparkSession) = {
+      val useDF = sparkSession
+        .read
+        .format("csv")
+        .option("header", "true")
+        .load("input.csv")
+
+        val model = PipelineModel.read.load("aircraft-model")
+        val predictions = model.transform(useDF)
+        predictions.select("PREDICTED_AIRCRAFT", "features").show()
+    }
+
+  /*
+    def useTargetModel(sparkSession: SparkSession) = {
+      val useDF = sparkSession
+        .read
+        .format("csv")
+        .option("header", "true")
+        .load("input.csv")
+
+
+      val model = PipelineModel.read.load("target-model")
+      val predictions = model.transform(useDF)
+      predictions.select("PREDICTED_TARGET", "features").show()
+    }*/
+
+    def createAndSaveAircraftModel(sparkConf: SparkConf, sparkSession: SparkSession) = {
+      //Load csv file
+      val rawDataFrame = sparkSession
+        .read
+        .format("csv")
+        .option("header", "true")
+        .load("small.csv")
+      //.load("hdfs://columbia:47481/data/THOR_Vietnam_Bombing_Operations.csv")
+
+      //And transform it....
+      val filtered = rawDataFrame.filter("COUNTRYFLYINGMISSION is not null")
+        .filter("VALID_AIRCRAFT_ROOT is not null")
+        .filter("WEAPONTYPE is not null")
+        .filter("FLTHOURS is not null")
+        .filter("PERIODOFDAY is not null")
+        .filter("TGTTYPE is not null")
+        .filter("WEAPONSLOADEDWEIGHT is not null")
+
+
+      //By selecting features...
+      val countryIndexed = new StringIndexer()
+        .setInputCol("COUNTRYFLYINGMISSION")
+        .setOutputCol("countryIndexed")
+        .fit(filtered)
+
+      val targetIndexed = new StringIndexer()
+        .setInputCol("TGTTYPE")
+        .setOutputCol("targetIndexed")
+        .fit(filtered)
+
+      val weaponIndexed = new StringIndexer()
+        .setInputCol("WEAPONTYPE")
+        .setOutputCol("weaponIndexed")
+        .fit(filtered)
+
+      val hoursIndexed = new StringIndexer()
+        .setInputCol("FLTHOURS")
+        .setOutputCol("hoursIndexed")
+        .fit(filtered)
+
+      val periodIndexed = new StringIndexer()
+        .setInputCol("PERIODOFDAY")
+        .setOutputCol("periodIndexed")
+        .fit(filtered)
+
+      val weaponWeightIndexed = new StringIndexer()
+        .setInputCol("WEAPONSLOADEDWEIGHT")
+        .setOutputCol("weaponWeightIndexed")
+        .fit(filtered)
+
+      //Add indexing on the labels...
+      val labelIndexer = new StringIndexer()
+        .setInputCol("VALID_AIRCRAFT_ROOT")
+        .setOutputCol("labels")
+        .fit(filtered)
+
+      val assembler  = new VectorAssembler()
+        .setInputCols(Array("countryIndexed", "targetIndexed", "weaponIndexed", "hoursIndexed", "periodIndexed", "weaponWeightIndexed"))
+        .setOutputCol("features")
+
+
+      //Transforming data complete
+      //-----------------------------------------------------
+
+
+      val Array(trainingData, testData) = filtered.randomSplit(Array(0.7, 0.3))
+
+
+      val labelConverter = new IndexToString()
+        .setInputCol("prediction")
+        .setOutputCol("PREDICTED_AIRCRAFT")
+        .setLabels(labelIndexer.labels)
+
+      val trainer = new MultilayerPerceptronClassifier()
+        .setLayers(Array[Int](6, 30, 30, 50))
+        .setBlockSize(128)
+        .setSeed(1234L)
+        .setMaxIter(100)
+        .setLabelCol("labels")
+        .setFeaturesCol("features")
+
+      val transformPipeline = new Pipeline()
+        .setStages(Array(countryIndexed, targetIndexed, weaponIndexed, hoursIndexed, periodIndexed, weaponWeightIndexed))
+        .fit(trainingData)
+
+      val pipeline = new Pipeline()
+        .setStages(Array(labelIndexer, transformPipeline, assembler, trainer, labelConverter))
+
+      val model = pipeline.fit(trainingData)
+
+
+      val predictions = model.transform(testData)
+
+      val evaluator = new MulticlassClassificationEvaluator()
+        .setLabelCol("labels")
+        .setPredictionCol("prediction")
+        .setMetricName("accuracy")
+
+      val accuracy = evaluator.evaluate(predictions)
+      model.write.overwrite().save("aircraft-model")
+
+
+      println("The model is " + accuracy + "% accurate.")
+    }
+
+
+  /*
+  def createAndSaveTargetModel(sparkConf: SparkConf, sparkSession: SparkSession) = {
+    //Load csv file
+    val rawDataFrame = sparkSession
+      .read
+      .format("csv")
+      .option("header", "true")
+      .load("small.csv")
+    //.load("hdfs://columbia:47481/data/THOR_Vietnam_Bombing_Operations.csv")
+
+    //And transform it....
+    val filtered = rawDataFrame.filter("COUNTRYFLYINGMISSION is not null")
+      .filter("VALID_AIRCRAFT_ROOT is not null")
+      .filter("WEAPONTYPE is not null")
+      .filter("FLTHOURS is not null")
+      .filter("PERIODOFDAY is not null")
+      .filter("TGTTYPE is not null")
+      .filter("WEAPONSLOADEDWEIGHT is not null")
+
+
+    //By selecting features...
+    val countryIndexed = new StringIndexer()
+      .setInputCol("COUNTRYFLYINGMISSION")
+      .setOutputCol("countryIndexed")
+      .fit(filtered)
+
+    val airplaneIndexed = new StringIndexer()
+      .setInputCol("VALID_AIRCRAFT_ROOT")
+      .setOutputCol("aircraftIndexed")
+      .fit(filtered)
+
+    val weaponIndexed = new StringIndexer()
+      .setInputCol("WEAPONTYPE")
+      .setOutputCol("weaponIndexed")
+      .fit(filtered)
+
+    val hoursIndexed = new StringIndexer()
+      .setInputCol("FLTHOURS")
+      .setOutputCol("hoursIndexed")
+      .fit(filtered)
+
+    val periodIndexed = new StringIndexer()
+      .setInputCol("PERIODOFDAY")
+      .setOutputCol("periodIndexed")
+      .fit(filtered)
+
+    val weaponWeightIndexed = new StringIndexer()
+      .setInputCol("WEAPONSLOADEDWEIGHT")
+      .setOutputCol("weaponWeightIndexed")
+      .fit(filtered)
+
+    val labelIndexed = new StringIndexer()
+      .setInputCol("TGTTYPE")
+      .setOutputCol("labels")
+      .fit(filtered)
+
+    val assembler  = new VectorAssembler()
+      .setInputCols(Array("countryIndexed", "aircraftIndexed", "weaponIndexed", "hoursIndexed", "periodIndexed", "weaponWeightIndexed"))
+      .setOutputCol("features")
+
+
+    //Transforming data complete
+    //-----------------------------------------------------
+
+
+    val Array(trainingData, testData) = filtered.randomSplit(Array(0.7, 0.3))
+
+
+    val labelConverter = new IndexToString()
+      .setInputCol("prediction")
+      .setOutputCol("PREDICTED_TARGET")
+      .setLabels(labelIndexed.labels)
+
+    val trainer = new MultilayerPerceptronClassifier()
+      .setLayers(Array[Int](6, 30, 30, 50))
+      .setBlockSize(128)
+      .setSeed(1234L)
+      .setMaxIter(100)
+      .setLabelCol("labels")
+      .setFeaturesCol("features")
+
+    val transformPipeline = new Pipeline()
+      .setStages(Array(countryIndexed, airplaneIndexed, weaponIndexed, hoursIndexed, periodIndexed, weaponWeightIndexed))
+      .fit(trainingData)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(labelIndexed, transformPipeline, assembler, trainer, labelConverter))
+
+    val model = pipeline.fit(trainingData)
+
+
+    val predictions = model.transform(testData)
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("labels")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+
+    val accuracy = evaluator.evaluate(predictions)
+    model.write.overwrite().save("target-model")
+
+
+    println("The model is " + accuracy + "% accurate.")
+  }*/
 }
